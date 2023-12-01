0f3bc8303e31d0e19540ea472a2a2afdb0738cf0
Jay Allison <jay.allison@acuitybrands.com>
2023-12-01 12:57:43 -0600

    Adding types

--- day01/puzzle1.py
+++ day01/puzzle1.py
@@ -4,15 +4,16 @@ import re
 input_filename = 'input.txt'
 
 # use a regular expression to find a single digit in the string
-digit_regex = re.compile(r'\d')
+digit_regex: re.Pattern = re.compile(r'\d')
 
-the_sum = 0
+the_sum: int = 0
 
+line: str
 for line in open(input_filename):
     # using findall() gives us a list[] of all of the matches back,
     # so that we can then easily ask for the first one and the last one
-    digits = digit_regex.findall(line)
-    number_string = digits[0] + digits[-1]
+    digits: list[str] = digit_regex.findall(line)
+    number_string: str = digits[0] + digits[-1]
     the_sum += int(number_string)
 
 print(the_sum)
--- day01/puzzle2.py
+++ day01/puzzle2.py
@@ -1,17 +1,17 @@
 import re
 
-# input_filename = 'sample_data2'
-input_filename = 'input.txt'
+# input_filename: str = 'sample_data2'
+input_filename: str = 'input.txt'
 
 # use a regular expression to find all single digits in the string by number and *most* single digits by name
 # ex: searching forward, this will find 'eight' but not 'two' in eightwo
 #     searching in reverse, this will find 'two' but not 'eight' in eightwo
 # that's not perfect, but it's good enough for this solution, because we don't need very digit, just first & last
-digit_regex_forward = re.compile(r'\d|one|two|three|four|five|six|seven|eight|nine')
-digit_regex_reverse = re.compile(r'\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin')
+digit_regex_forward: re.Pattern = re.compile(r'\d|one|two|three|four|five|six|seven|eight|nine')
+digit_regex_reverse: re.Pattern = re.compile(r'\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin')
 
 # map the digit by name (forward or backward) or by digit string to the digit string
-conversion = {
+conversion: dict[str, str] = {
     '1': '1',
     'one': '1',
     'eno': '1',
@@ -41,14 +41,15 @@ conversion = {
     'enin': '9',
 }
 
-the_sum = 0
+the_sum: int = 0
 
+line: str
 for line in open(input_filename):
     # use regex to search the string for the first digit, either the number or the name
-    first_digit = digit_regex_forward.search(line)[0]
+    first_digit: str = digit_regex_forward.search(line)[0]
     # use regex search the reversed string for the last digit, either the number or the name
-    last_digit = digit_regex_reverse.search(line[::-1])[0]
-    number_string = conversion[first_digit] + conversion[last_digit]
+    last_digit: str = digit_regex_reverse.search(line[::-1])[0]
+    number_string: str = conversion[first_digit] + conversion[last_digit]
     the_sum += int(number_string)
 
 print(the_sum)

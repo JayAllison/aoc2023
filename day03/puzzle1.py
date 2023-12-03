5923b991e90de5651b655f7848f52c8027eb5cf1
Jay Allison <jay.allison@acuitybrands.com>
2023-12-03 14:29:31 -0600

    added types, plus a bit of simplification

--- day03/puzzle1.py
+++ day03/puzzle1.py
@@ -1,15 +1,17 @@
 import re
+import itertools
 from pprint import pprint
 
-# input_filename = 'sample_data'
-# input_filename = 'sample2'
-input_filename = 'input.txt'
+# input_filename: str = 'sample_data'
+# input_filename: str = 'sample2'
+input_filename: str = 'input.txt'
 
 # read in the input and find the bounds
 schematic: list[str] = [line.rstrip() for line in open(input_filename)]
-x_max = len(schematic[0])  # number of columns
-y_max = len(schematic)  # number of rows
+X_MAX: int = len(schematic[0])  # number of columns
+Y_MAX: int = len(schematic)  # number of rows
 
+# the unique identifier of each part number is its starting position
 # store every position containing a digit, mapping to the starting point of that full part number
 part_number_positions: dict[tuple[int, int]: tuple[int, int]] = {}
 
@@ -17,11 +19,11 @@ part_number_positions: dict[tuple[int, int]: tuple[int, int]] = {}
 part_numbers: dict[tuple[int, int]: int] = {}
 
 # Step 1: search for all part numbers, and store off their locations and values
-part_number_finder = re.compile(r'[0-9]+')
-for y in range(y_max):
-    part_number_search = part_number_finder.finditer(schematic[y])
-    for part_number_found in part_number_search:
-        part_number_start = (part_number_found.start(), y)
+part_number_finder: re.Pattern = re.compile(r'[0-9]+')
+for y in range(Y_MAX):
+    part_number_found: re.Match
+    for part_number_found in part_number_finder.finditer(schematic[y]):
+        part_number_start: tuple[int, int] = (part_number_found.start(), y)
         part_numbers[part_number_start] = int(part_number_found[0])
         for x in range(part_number_found.start(), part_number_found.end(), 1):
             part_number_positions[(x, y)] = part_number_start
@@ -29,23 +31,20 @@ for y in range(y_max):
 print(f'Found {len(part_numbers)} part numbers.')
 
 # store all found part number starting positions in a set, so we only include each part number once
-adjacent_part_number_starting_positions = set()
+adjacent_part_number_starting_positions: set[tuple[int, int]] = set()
 
 # Step 2: find symbols and check adjacent positions for the presence of a part number
-not_symbols = '0123456789.'
-symbol_count = 0
-for y in range(y_max):
-    for x in range(x_max):
-        if schematic[y][x] not in not_symbols:
-            symbol_count += 1
-            for y1 in range(y-1, y+2, 1):
-                for x1 in range(x-1, x+2, 1):
-                    if (x1, y1) in part_number_positions:
-                        adjacent_part_number_starting_positions.add(part_number_positions[(x1, y1)])
+symbol_count: int = 0
+for x, y in itertools.product(range(X_MAX), range(Y_MAX)):
+    if schematic[y][x] not in '0123456789.':
+        symbol_count += 1
+        for x1, y1 in itertools.product(range(x-1, x+2, 1), range(y-1, y+2, 1)):
+            if (x1, y1) in part_number_positions:
+                adjacent_part_number_starting_positions.add(part_number_positions[(x1, y1)])
 
 print(f'Found {symbol_count} symbols.')
 print(f'Found {len(adjacent_part_number_starting_positions)} adjacent part numbers.')
 
 # Step 3: sum the part numbers that we identified
-adjacent_part_numbers = [part_numbers[p] for p in adjacent_part_number_starting_positions]
+adjacent_part_numbers: list[int] = [part_numbers[p] for p in adjacent_part_number_starting_positions]
 print(f'Adjacent part number sum = {sum(adjacent_part_numbers)}')
--- day03/puzzle2.py
+++ day03/puzzle2.py
@@ -1,4 +1,5 @@
 import re
+import itertools
 from pprint import pprint
 
 # input_filename = 'sample_data'
@@ -10,6 +11,7 @@ schematic: list[str] = [line.rstrip() for line in open(input_filename)]
 x_max = len(schematic[0])  # number of columns
 y_max = len(schematic)  # number of rows
 
+# the unique identifier of each part number is its starting position
 # store every position containing a digit, mapping to the starting point of that full part number
 part_number_positions: dict[tuple[int, int]: tuple[int, int]] = {}
 
@@ -17,11 +19,11 @@ part_number_positions: dict[tuple[int, int]: tuple[int, int]] = {}
 part_numbers: dict[tuple[int, int]: int] = {}
 
 # Step 1: search for all part numbers, and store off their locations and values
-part_number_finder = re.compile(r'[0-9]+')
+part_number_finder: re.Pattern = re.compile(r'[0-9]+')
 for y in range(y_max):
-    part_number_search = part_number_finder.finditer(schematic[y])
-    for part_number_found in part_number_search:
-        part_number_start = (part_number_found.start(), y)
+    part_number_found: re.Match
+    for part_number_found in part_number_finder.finditer(schematic[y]):
+        part_number_start: tuple[int, int] = (part_number_found.start(), y)
         part_numbers[part_number_start] = int(part_number_found[0])
         for x in range(part_number_found.start(), part_number_found.end(), 1):
             part_number_positions[(x, y)] = part_number_start
@@ -32,19 +34,17 @@ print(f'Found {len(part_numbers)} part numbers.')
 gear_ratios: list[int] = []
 
 # Step 2: find gears and gear ratios
-gear_count = 0
-for y in range(y_max):
-    for x in range(x_max):
-        if schematic[y][x] == '*':
-            # let the set help us keep track of how many unique part numbers are adjacent
-            adjacent_part_numbers: set[tuple[int, int]] = set()
-            for y1 in range(y-1, y+2, 1):
-                for x1 in range(x-1, x+2, 1):
-                    if (x1, y1) in part_number_positions:
-                        adjacent_part_numbers.add(part_number_positions[(x1,y1)])
-            if len(adjacent_part_numbers) == 2:
-                gear_count += 1
-                gear_ratios.append(part_numbers[adjacent_part_numbers.pop()] * part_numbers[adjacent_part_numbers.pop()])
+gear_count: int = 0
+for x, y in itertools.product(range(x_max), range(y_max)):
+    if schematic[y][x] == '*':
+        # let the set help us keep track of how many unique part numbers are adjacent
+        adjacent_part_numbers: set[tuple[int, int]] = set()
+        for x1, y1 in itertools.product(range(x-1, x+2, 1), range(y-1, y+2, 1)):
+            if (x1, y1) in part_number_positions:
+                adjacent_part_numbers.add(part_number_positions[(x1,y1)])
+        if len(adjacent_part_numbers) == 2:
+            gear_count += 1
+            gear_ratios.append(part_numbers[adjacent_part_numbers.pop()] * part_numbers[adjacent_part_numbers.pop()])
 
 print(f'Found {gear_count} gears.')
 

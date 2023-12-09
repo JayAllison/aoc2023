9418604cc1b74ea9770d589daa13a7dbeb315c3d
Jay Allison <jay.allison@acuitybrands.com>
2023-12-09 07:58:23 -0600

    Day 9: first pass at working solutions

---
+++ day09/puzzle2.py
@@ -0,0 +1,33 @@
+import pprint
+
+# input_filename = 'sample_data'
+input_filename: str = 'input.txt'
+
+histories: list[list[int]] = [[int(i) for i in line.split()] for line in open(input_filename).readlines()]
+
+# evaluate each history list that we were given
+for history in histories:
+    # part 2 asks us to work to the left, so let's just reverse the input, then our existing code will just work
+    history.reverse()
+    deltas: list[list[int]] = [history]
+    di = 0
+
+    # find the deltas for each stage of the process, until the deltas for the entire stage are all zero
+    while True:
+        next_delta: list[int] = []
+        deltas.append(next_delta)
+        for i in range(1, len(deltas[di]), 1):
+            next_delta.append(deltas[di][i] - deltas[di][i-1])
+        di += 1
+        if not any(next_delta):
+            break
+
+    # working backwards, predict the previous value at each stage, until we have a projection for the original history
+    deltas[-1].append(0)
+    for si in range(len(deltas)-2, 0, -1):
+        deltas[si-1].append(deltas[si-1][-1] + deltas[si][-1])
+
+    pprint.pprint(deltas)
+
+# sum up the new predicted value on each history
+print(sum([h[-1] for h in histories]))

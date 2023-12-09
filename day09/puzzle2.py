59f865f13a7718d6fef72bb752890661db3d6c02
Jay Allison <jay.allison@acuitybrands.com>
2023-12-09 08:17:19 -0600

    comments, types, clarifications

--- day09/puzzle2.py
+++ day09/puzzle2.py
@@ -1,33 +1,31 @@
-import pprint
-
-# input_filename = 'sample_data'
+# input_filename: str = 'sample_data'
 input_filename: str = 'input.txt'
 
+# read in each line, split it into digit strings, then convert each string to an int, and store as a list of ints
 histories: list[list[int]] = [[int(i) for i in line.split()] for line in open(input_filename).readlines()]
 
-# evaluate each history list that we were given
+# predict each history list that we were given
 for history in histories:
-    # part 2 asks us to work to the left, so let's just reverse the input, then our existing code will just work
+    # part 2 says to work right to left, so let's simply reverse the input, then our existing L->R code will just work
     history.reverse()
+
     deltas: list[list[int]] = [history]
-    di = 0
+    current_delta: list[int] = deltas[0]
 
     # find the deltas for each stage of the process, until the deltas for the entire stage are all zero
     while True:
         next_delta: list[int] = []
         deltas.append(next_delta)
-        for i in range(1, len(deltas[di]), 1):
-            next_delta.append(deltas[di][i] - deltas[di][i-1])
-        di += 1
+        for i in range(1, len(current_delta), 1):
+            next_delta.append(current_delta[i] - current_delta[i-1])
+        current_delta = next_delta
         if not any(next_delta):
             break
 
-    # working backwards, predict the previous value at each stage, until we have a projection for the original history
+    # predict the next value at each stage, until we have a prediction for the original history
     deltas[-1].append(0)
-    for si in range(len(deltas)-2, 0, -1):
-        deltas[si-1].append(deltas[si-1][-1] + deltas[si][-1])
+    for di in range(len(deltas) - 2, 0, -1):
+        deltas[di - 1].append(deltas[di - 1][-1] + deltas[di][-1])
 
-    pprint.pprint(deltas)
-
-# sum up the new predicted value on each history
+# sum up the new predicted values from each history
 print(sum([h[-1] for h in histories]))

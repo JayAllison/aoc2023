a3e286fa3fe39c6b86e74285f2c5d4f782bbb618
Jay Allison <jay.allison@acuitybrands.com>
2023-12-18 14:54:56 -0600

    Updating giftcard script to cache session ID and to display number of qualifiers

--- gift_card_report.py
+++ gift_card_report.py
@@ -1,17 +1,27 @@
 import getpass
 # import pprint
+import os
+import json
 import requests
+cache_filename = '.giftcard_cache'
 
-# ask the user for an authenticated session ID, rather than hardcoding it or building auth workflow here
-print('\nPlease log in via https://www.adventofcode.com/ then paste the authenticated Session ID here.\n')
-session_id = getpass.getpass('Advent of Code Session ID: ')
-print()
+cached_data = {}
+if os.path.exists(cache_filename):
+    cached_data = json.load(open(cache_filename))
+
+if 'session_id' not in cached_data:
+    # ask the user for an authenticated session ID, rather than hardcoding it or building auth workflow here
+    print('\nPlease log in via https://www.adventofcode.com/ then paste the authenticated Session ID here.\n')
+    cached_data['session_id'] = getpass.getpass('Advent of Code Session ID: ')
+    print()
+
+    json.dump(cached_data, open(cache_filename, 'w'))
 
 giftcard_star_threshold = 25
 leaderboard_url = 'https://adventofcode.com/2023/leaderboard/private/view/210077.json'
 
 # use the session ID to get the data from the specified leaderboard
-leaderboard_response = requests.get(leaderboard_url, headers={'Cookie': f'session={session_id};'})
+leaderboard_response = requests.get(leaderboard_url, headers={'Cookie': f'session={cached_data['session_id']};'})
 
 try:
     leaderboard_response.raise_for_status()
@@ -32,12 +42,16 @@ for member_id in leaderboard['members']:
     if star_count >= 25:
         gift_card_earners[leaderboard['members'][member_id]['name']] = star_count
 
-print('Gift card earners (alphabetical order):')
+print('Gift Card Earners (alphabetical order):')
 print('--------------------------------------')
 print()
 
 # sort output by star count high to low, then alphabetically by name
+count = 0
 for star_counts in sorted(set(gift_card_earners.values()), reverse=True):
     for earner in sorted(gift_card_earners.keys()):
         if star_counts == gift_card_earners[earner]:
             print(f'{earner}: {gift_card_earners[earner]}')
+            count += 1
+
+print(f'\nTotal qualifiers: {count}')

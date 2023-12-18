adba5c50ac201a585585af330582459c7383b1d4
Jay Allison <jay.allison@acuitybrands.com>
2023-12-18 14:03:29 -0600

    comments, types, clarifications, corrections

--- day18/puzzle1.py
+++ day18/puzzle1.py
@@ -1,61 +1,65 @@
 from collections import deque
-import pprint
-import re
+# import pprint
 
 # input_filename: str = 'sample_data'
 input_filename: str = 'input.txt'
 
-dig_plan = [line.rstrip().split() for line in open(input_filename)]
+dig_plan: list[list[str]] = [line.rstrip().split() for line in open(input_filename)]
 
-digger_path = []
+digger_path: list[tuple] = []
 
-current = (0, 0)
+# lookup table for deltas to move in the specified direction
+MOVES = {
+    'U': (-1, 0),
+    'D': (+1, 0),
+    'L': (0, -1),
+    'R': (0, +1),
+}
+
+# trace out the digger path
+current: tuple[int, int] = (0, 0)
 for instruction in dig_plan:
     direction, length, color = instruction
     for step in range(int(length)):
         y, x = current
-        match direction:
-            case 'U':
-                y -= 1
-            case 'D':
-                y += 1
-            case 'L':
-                x += 1
-            case 'R':
-                x -= 1
-        current = (y, x)
+        dy, dx = MOVES[direction]
+        current = (y + dy, x + dx)
         digger_path.append(current)
 
 # pprint.pprint(digger_path)
 
-all_ys = [coord[0] for coord in digger_path]
-min_y = min(all_ys)
-max_y = max(all_ys)
+# the digger path coordinates are relative, not absolute, so we need to offset them into a grid of positive numbers
+all_ys: list[int] = [coord[0] for coord in digger_path]
+min_y: int = min(all_ys)
+max_y: int = max(all_ys)
 
-all_xs = [coord[1] for coord in digger_path]
-min_x = min(all_xs)
-max_x = max(all_xs)
+all_xs: list[int] = [coord[1] for coord in digger_path]
+min_x: int = min(all_xs)
+max_x: int = max(all_xs)
 
-print(min_y, max_y)
-print(min_x, max_x)
+# print(min_y, max_y)
+# print(min_x, max_x)
 
-dig_map = [['.' for x in range(min_x, max_x + 2, 1)] for y in range(min_y, max_y + 1, 1)]
+# lay out an absolute grid
+dig_map: list[list[str]] = [['.' for x in range(min_x, max_x + 2, 1)] for y in range(min_y, max_y + 1, 1)]
 
+# trace out the digger path in the grid
 for point in digger_path:
     y, x = point
     dig_map[y - min_y][x - min_x] = '#'
 
+# the output is too wide for my terminal - use a file instead
 with open('map_test.txt', 'w') as output:
     for row in dig_map:
         output.write(''.join(row) + '\n')
     output.write('\n')
 
-# a simple fill algorithm should work - let's assume path is clockwise, so 1 D and 1 R from start should be good
-max_height = len(dig_map)
-max_width = len(dig_map[0])
+max_height: int = len(dig_map)
+max_width: int = len(dig_map[0])
 
-# because of how I did the offsets, mine is mirrored, so down one and in one is really down out and out one
-fill_from = deque([(-min_y + 1, -min_x - 1)])
+# a simple fill algorithm should work - let's assume path is clockwise, so 1 ⬇ and 1 ➡ from start should be good
+# relative start was (0, 0), so offsets to absolute start is (-min_y, -min_x)
+fill_from: deque = deque([(-min_y + 1, -min_x + 1)])
 while fill_from:
     y, x = fill_from.popleft()
     # print(f'Checking around ({y}, {x}):')
@@ -66,9 +70,10 @@ while fill_from:
                 dig_map[y1][x1] = '#'
                 fill_from.append((y1, x1))
 
+# the output is too wide for my terminal - use a file instead
 with open('map_test.txt', 'a') as output:
     for row in dig_map:
         output.write(''.join(row) + '\n')
 
-count = sum([row.count('#') for row in dig_map])
-print(count)
+# brute force: count out the spots
+print(sum([row.count('#') for row in dig_map]))

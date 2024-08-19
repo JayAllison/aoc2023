67c03a8211b1d59b07e57c565f7768eb41cda718
Andrew Tomai <andrew.tomai@acuitybrands.com>
2024-08-19 10:17:26 -0700

    d1p1

--- day01/day01_part1.js
+++ day01/day01_part1.js
@@ -7,13 +7,17 @@ const fs = require('fs');
 const lines = fs.readFileSync(filename, 'utf-8').split(/\r?\n/).filter(l => l.length > 0);
 // console.log(lines);
 
-let sum = 0;
 
-lines.forEach(line => {
+// This section of code is an accumulator -- I would use array.reduce instead of array.forEach
+// The general rule of thumb I use is "don't use forEach when another function will suffice".
+// I typically use forEach when I need a "side effect", like I/O
+const INITIAL_VALUE = 0
+
+const sum = lines.reduce((accumulator, line) => {
     const regex = /(\d)/;
     let calibration_value = parseInt(regex.exec(line)[1]) * 10 + 
         parseInt(regex.exec(line.split('').reverse().join(''))[1]);
-    sum += calibration_value;
-});
+    return accumulator + calibration_value;
+}, INITIAL_VALUE)
 
 console.log(sum);
